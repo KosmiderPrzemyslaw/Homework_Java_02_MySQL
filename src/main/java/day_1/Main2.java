1bc22a674a5139353538c82bb50e2fe6eb02d8f5
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-11-29 16:39:36 +0100

    set charset and collate

--- src/main/java/day_1/Main2.java
+++ src/main/java/day_1/Main2.java
@@ -3,45 +3,58 @@ package day_1;
 public class Main2 {
 
     static String createTableProducts = "CREATE TABLE IF NOT EXISTS products " +
-            "(product_id INT AUTO_INCREMENT," +
+            "(product_id INT UNSIGNED NOT NULL AUTO_INCREMENT," +
             "product_name VARCHAR(255)," +
             "description VARCHAR(255)," +
             "price DECIMAL(5,2), " +
-            "PRIMARY KEY (product_id))";
+            "PRIMARY KEY (product_id))" +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTableOrders = "CREATE TABLE IF NOT EXISTS orders " +
-            "(order_id INT AUTO_INCREMENT," +
+            "(order_id INT unsigned NOT NULL AUTO_INCREMENT," +
             "description VARCHAR(255)," +
-            "PRIMARY KEY (order_id))";
+            "PRIMARY KEY (order_id))" +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTableClients = "CREATE TABLE IF NOT EXISTS clients " +
-            "(client_id INT AUTO_INCREMENT, " +
+            "(client_id INT unsigned NOT NULL AUTO_INCREMENT, " +
             "name VARCHAR(255)," +
             "surname VARCHAR (255)," +
-            "PRIMARY KEY(client_id))";
+            "PRIMARY KEY(client_id))" +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTableCinemas = "CREATE TABLE IF NOT EXISTS cinemas " +
-            "(cinema_id INT AUTO_INCREMENT," +
+            "(cinema_id INT unsigned NOT NULL AUTO_INCREMENT," +
             "name VARCHAR(255)," +
             "address VARCHAR(255)," +
-            "PRIMARY KEY(cinema_id))";
+            "PRIMARY KEY(cinema_id))" +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTableMovies = "CREATE TABLE IF NOT EXISTS movies " +
-            "(movie_id INT AUTO_INCREMENT," +
+            "(movie_id INT unsigned NOT NULL AUTO_INCREMENT," +
             "title VARCHAR(255)," +
             "description VARCHAR(255)," +
             "rating DECIMAL(3,2)," +
-            "PRIMARY KEY(movie_id))";
+            "PRIMARY KEY(movie_id))" +
+            "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTableTickets = "CREATE TABLE IF NOT EXISTS tickets " +
-            "(ticket_id INT AUTO_INCREMENT," +
+            "(ticket_id INT unsigned NOT NULL AUTO_INCREMENT," +
             "quantity INT," +
             "price DECIMAL(3,2)," +
-            "PRIMARY KEY(ticket_id))";
+            "PRIMARY KEY(ticket_id))" +
+            "ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci";
+
 
     static String createTablePayments = "CREATE TABLE IF NOT EXISTS payments " +
-            "(payment_id INT AUTO_INCREMENT," +
+            "(payment_id INT unsigned NOT NULL AUTO_INCREMENT," +
             "type VARCHAR(255)," +
             "payment_date DATE," +
-            "PRIMARY KEY(payment_id))";
+            "PRIMARY KEY(payment_id))" +
+            "ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=101";
 }
