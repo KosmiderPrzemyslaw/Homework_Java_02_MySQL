1575b3a1b994e1ca831790815d1f358b5302aa08
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-01 22:20:33 +0100

    get data from database

--- src/main/java/day_1/Main7.java
+++ src/main/java/day_1/Main7.java
@@ -6,19 +6,20 @@ import java.sql.*;
 
 public class Main7 {
 
+    public static String selectAllTickets = "SELECT SUM(products_ex.tickets.quantity) as sumOfTickets FROM products_ex.tickets";
+    public static String selectAllPayments = "SELECT products_ex.payments.type AS paymentsByType FROM products_ex.payments GROUP BY type";
+
     public static void main(String[] args) {
-        try {
-            Statement statement = getConnection().createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        getDataFromDatabase();
     }
 
-    private static void getDataFromDatabase(String statement) {
+    private static void getDataFromDatabase() {
         try {
-            PreparedStatement preparedStatement = getConnection().prepareStatement(statement);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            PrintUtil.print(resultSet);
+            ResultSet resultSet1 = getConnection().createStatement().executeQuery(selectAllTickets);
+            PrintUtil.print(resultSet1, "sumOfTickets");
+
+            ResultSet resultSet = getConnection().createStatement().executeQuery(selectAllPayments);
+            PrintUtil.print(resultSet, "paymentsByType");
 
         } catch (SQLException e) {
             e.printStackTrace();
