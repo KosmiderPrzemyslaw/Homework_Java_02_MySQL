babc1dd5447a36239ca3eca1a0237d9820e2ad73
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-01 22:31:24 +0100

    select last 5 payments

--- src/main/java/day_1/Main7.java
+++ src/main/java/day_1/Main7.java
@@ -9,6 +9,7 @@ public class Main7 {
     public static String selectAllTickets = "SELECT SUM(products_ex.tickets.quantity) as sumOfTickets FROM products_ex.tickets";
     public static String selectAllPayments = "SELECT products_ex.payments.type, COUNT(*) AS paymentsByType FROM products_ex.payments GROUP BY type";
     public static String selectAvgOneTicketPriceFromTickets = "SELECT AVG(products_ex.tickets.price) as avgPrice FROM products_ex.tickets";
+    public static String selectLastFivePaymentsStartingFromSecond = "SELECT products_ex.payments.payment_date FROM products_ex.payments ORDER BY payment_date DESC, payment_id DESC LIMIT 1,5";
 
     public static void main(String[] args) {
         getDataFromDatabase();
@@ -24,6 +25,9 @@ public class Main7 {
 
             ResultSet resultSet2 = getConnection().createStatement().executeQuery(selectAvgOneTicketPriceFromTickets);
             PrintUtil.print(resultSet2, "avgPrice");
+
+            ResultSet resultSet3 = getConnection().createStatement().executeQuery(selectLastFivePaymentsStartingFromSecond);
+            PrintUtil.print(resultSet3, "payment_date");
 
         } catch (SQLException e) {
             e.printStackTrace();
