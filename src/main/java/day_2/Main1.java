40da05dd38511095aa5a6426b3d20667c91c16ed
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-05 21:32:31 +0100

    remove movie by id

--- src/main/java/day_2/Main1.java
+++ src/main/java/day_2/Main1.java
@@ -4,18 +4,20 @@ import DBConnection.DBConnection;
 import day_1.PrintUtil;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj id filmu do usunięcia: ");
+
 
         try {
             Connection connection = new DBConnection().getConnection();
             printAllMovies(connection);
+            int movieIdToRemove = getMovieIdToRemove();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -30,4 +32,18 @@ public class Main1 {
 
     }
 
+    private static int getMovieIdToRemove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj id filmu do usunięcia: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Podaj Id filmu do usunięcia! Wprowadź liczbę całkowitą!");
+        }
+        return scanner.nextInt();
+    }
+
+    private static void removeMovieById(int id, Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM products_ex.movies WHERE movie_id=?");
+        preparedStatement.setInt(1, );
+    }
 }
