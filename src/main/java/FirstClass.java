93169b7eb2703d8850d9dc8bcf7c12b93a4777c6
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-11 20:43:18 +0100

    tests

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -14,6 +14,14 @@ public class FirstClass {
                     connection.prepareStatement("SELECT * from homework_day1_mysql.offers where promoted = 0 and price > 50000 and title like '%l' ");
             ResultSet resultSet = preparedStatement.executeQuery();
             PrintUtil.print(resultSet, "title", "expire");
+
+            connection.prepareStatement("SELECT * FROM homework_day1_mysql.offers where price < price*2 and promoted = 1 and promoted_to > CURRENT_DATE");
+            ResultSet resultSet2 = preparedStatement.executeQuery();
+            PrintUtil.print(resultSet2, "title", "price", "expire");
+
+            connection.prepareStatement("SELECT COUNT(*) AS sum_active FROM homework_day1_mysql.offers WHERE status = 1 and expire > NOW()");
+            ResultSet resultSet3 = preparedStatement.executeQuery();
+            PrintUtil.print(resultSet3, "status", "title");
         } catch (SQLException e) {
             e.printStackTrace();
         }
