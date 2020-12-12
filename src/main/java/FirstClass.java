5d578cc74b0a2fa2557f11c78cc2b91b1592ea47
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-12 06:17:34 +0100

    done task 14

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -28,6 +28,31 @@ public class FirstClass {
             PreparedStatement preparedStatement4 = connection.prepareStatement("SELECT homework_day1_mysql.offers.title AS offersTitle FROM homework_day1_mysql.offers ORDER BY id DESC");
             printResults(preparedStatement4, "offersTitle");
 
+            PreparedStatement preparedStatement5 = connection.prepareStatement("SELECT COUNT(*) AS sum_active FROM homework_day1_mysql.offers WHERE expire > NOW() and status = 1");
+            printResults(preparedStatement5, "sum_active");
+
+            PreparedStatement preparedStatement6 = connection.prepareStatement
+                    ("SELECT owner, COUNT(*) " +
+                            "AS sum_user " +
+                            "FROM homework_day1_mysql.offers " +
+                            "WHERE status = 1 and expire > NOW() GROUP BY owner");
+            printResults(preparedStatement6, "owner","sum_user");
+
+            PreparedStatement preparedStatement7 = connection.prepareStatement
+                    ("SELECT status as type, COUNT(status) as counter " +
+                            "FROM homework_day1_mysql.offers " +
+                            "WHERE status = 1 OR status = 2 " +
+                            "GROUP BY status " +
+                            "ORDER BY type");
+            printResults(preparedStatement7, "type", "counter");
+
+            PreparedStatement preparedStatement8 = connection.prepareStatement
+                    ("SELECT SUM(price) AS sum_nonactive_nonpromoted " +
+                            "FROM homework_day1_mysql.offers " +
+                            "WHERE status = 1 AND promoted = 0 and expire < NOW()");
+            printResults(preparedStatement8, "sum_nonactive_nonpromoted");
+
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -38,6 +63,5 @@ public class FirstClass {
     public static void printResults(PreparedStatement preparedStatement , String... columnNames) throws SQLException {
         ResultSet resultSet = preparedStatement.executeQuery();
         PrintUtil.print(resultSet, columnNames);
-
     }
 }
