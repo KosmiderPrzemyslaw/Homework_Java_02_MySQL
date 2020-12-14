8528bb99858e3f1dfb8a5f6212551176ccabfb13
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-14 21:08:53 +0100

    next sql task

--- src/main/java/homework_day1/Main2.java
+++ src/main/java/homework_day1/Main2.java
@@ -32,9 +32,40 @@ public class Main2 {
                             "post_code CHAR(6)," +
                             "capital DECIMAL(7,2)," +
                             "rate DECIMAL(5,4)," +
-                            "created_ad DATETIME)");
+                            "created_ad DATETIME," +
+                            "FOREIGN KEY (user_id) REFERENCES homework_day1_mysql.users(id))");
 
             preparedStatement1.executeUpdate();
+
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement
+                    ("CREATE TABLE IF NOT EXISTS customers (" +
+                            "customer_id int not null auto_increment," +
+                            "name varchar(255) not null ," +
+                            "primary key(customer_id))" +
+                            "ENGINE = InnoDB CHARSET = utf8");
+            preparedStatement2.executeUpdate();
+
+            PreparedStatement preparedStatement3 = connection.prepareStatement
+                    ("CREATE TABLE  IF NOT EXISTS addresses(" +
+                            "addresses_id int not null ," +
+                            "street VARCHAR(255)," +
+                            "PRIMARY KEY (addresses_id)," +
+                            "FOREIGN KEY (addresses_id) references homework_day1_mysql.customers(customer_id) ON DELETE CASCADE ON UPDATE CASCADE)");
+            preparedStatement3.executeUpdate();
+//
+//            PreparedStatement preparedStatement2 = connection.prepareStatement
+//                    ("CREATE TABLE users_addresses" +
+//                            "(user_id int(10) NOT NULL," +
+//                            "city VARCHAR(50)," +
+//                            "street VARCHAR(50)," +
+//                            "house_nr VARCHAR(10)," +
+//                            "PRIMARY KEY (user_id)," +
+//                            "FOREIGN KEY (user_id) REFERENCES homework_day1_mysql.users(id))" +
+//                            "ENGINE=InnoDB DEFAULT CHARSET=utf8 ");
+//
+//            preparedStatement2.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/homework_day2/Main2.java
@@ -0,0 +1,4 @@
+package homework_day2;
+
+public class Main2 {
+}
